6537e04083c588433509794d8c1caaebb9e448c2
Yusuf Tekin <tekinjoseph@gmail.com>
2026-02-09 01:01:49 +0300

    Update git-archive.rb

--- Formula/git-archive.rb
+++ Formula/git-archive.rb
@@ -2,7 +2,7 @@ class GitArchive < Formula
   desc "Offline-first Git archiver with bundle, checksum, encryption and restore"
   homepage "https://github.com/ace1ofspades/git-archive"
   url "https://github.com/ace1ofspades/git-archive/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "098836803c71618a520917d8769806f5f23f47972c9255d92b3b8030ff4b1790"
+  sha256 "5d1f058587de23f768a3c805626ea384e988c23a6dbd10b84310c4468a81b2ff"
   license "MIT"
 
   def install
