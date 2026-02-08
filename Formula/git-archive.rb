2231005582ba67a73c728eb1cf18902955ae4b19
Yusuf Tekin <tekinjoseph@gmail.com>
2026-02-09 00:25:58 +0300

    Update git-archive.rb

--- Formula/git-archive.rb
+++ Formula/git-archive.rb
@@ -1,8 +1,8 @@
 class GitArchive < Formula
-  desc "Minimal offline Git archiver: bundle + sha256 + zip"
+  desc "Offline-first Git archiver with bundle, checksum, encryption and restore"
   homepage "https://github.com/ace1ofspades/git-archive"
-  url "https://github.com/ace1ofspades/git-archive/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "b3105b4c5aae515fd437452b45f6537bdc621e2f36b908bdac1f534bc6070daf"
+  url "https://github.com/ace1ofspades/git-archive/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "2d4907174d49938f4442b6fd28e477f1a7c171a7b6893ab79d61e28c45deab42"
   license "MIT"
 
   def install
@@ -10,6 +10,6 @@ class GitArchive < Formula
   end
 
   test do
-    assert_match "Usage:", shell_output("#{bin}/git-archive 2>&1", 0)
+    assert_match "git-archive 0.2.1", shell_output("#{bin}/git-archive --version")
   end
 end
